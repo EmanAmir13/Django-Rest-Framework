13094ea973f1d78f0b429c4c3599414b485e4e93
Eman Amir <eman.amir@flicken.io>
2024-01-11 12:31:45 +0500

    Implemnet views by APIVIEWS that are class base viwes

--- crudByFunctionbaseApi/v1/api/urls.py
+++ crudByFunctionbaseApi/v1/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('api/v1/student-api/', student_api, name='student_api'),
+    path('api/v1/student-api/', StudentAPI.as_view(), name='student_api'),
+    path('api/v1/student-api/<int:pk>/', StudentAPI.as_view(), name='student_api'),
 ]
