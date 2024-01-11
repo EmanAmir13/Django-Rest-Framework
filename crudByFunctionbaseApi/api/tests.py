6c69f773e03a352e06d3298a751c428df14ca90b
Eman Amir <eman.amir@flicken.io>
2024-01-11 11:42:46 +0500

    Write the test or api view

--- crudByFunctionbaseApi/api/tests.py
+++ crudByFunctionbaseApi/api/tests.py
@@ -1,3 +1,61 @@
-from django.test import TestCase
+import request
+import json
 
-# Create your tests here.
+import requests
+
+URL = "http://127.0.0.1:8000/api/v1/student-api/"
+
+
+def get_data(id=None):
+    data = {}
+    if id is not None:
+        data = {'id': id}
+    json_data = json.dumps(data)
+    r = requests.get(url=URL, data=json_data)
+    data = r.json()
+    print(data)
+
+
+# get_data(1)
+
+
+def post_data():
+    data = {
+        'id': 4,
+        'roll_no': 25,
+        'name': 'mahi',
+        'city': 'islamabad'
+    }
+    json_data = json.dumps(data)
+    r = requests.post(url=URL, data=json_data)
+    data = r.json()
+    print(data)
+
+
+# post_data()
+
+
+def update_data():
+    data = {
+        'id': 1,
+        'roll_no': 25,
+        'name': 'mano',
+        'city': 'Lahore'
+    }
+    json_data = json.dumps(data)
+    r = requests.put(url=URL, data=json_data)
+    data = r.json()
+    print(data)
+
+
+# update_data()
+
+
+def delete_data():
+    data = {'id': 4}
+    json_data = json.dumps(data)
+    r = requests.delete(url=URL, data=json_data)
+    data = r.json()
+    print(data)
+
+# delete_data()
