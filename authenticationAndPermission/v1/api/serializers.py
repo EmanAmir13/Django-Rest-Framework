361482307aa2988cf46b95412f6a18a17c08c860
Eman Amir <eman.amir@flicken.io>
2024-01-12 11:16:13 +0500

    Created the views for the model and it in url by help of router

--- authenticationAndPermission/v1/api/serializers.py
+++ authenticationAndPermission/v1/api/serializers.py
@@ -5,4 +5,4 @@ from api.models import Student
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
-        fields = ['name', 'roll_no', 'city']
+        fields = ['id', 'name', 'roll_no', 'city']
--- authenticationAndPermission/v1/api/urls.py
+++ authenticationAndPermission/v1/api/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path, include
 from .views import *
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+
+router.register('studentapi', StudentModelViewSet, basename='student')
+
 urlpatterns = [
-    # path('', ),
+    path('', include(router.urls)),
 ]
