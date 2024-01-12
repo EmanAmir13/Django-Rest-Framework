fa7614490ae950e299520a8e94e8ebda1b018eea
Eman Amir <eman.amir@flicken.io>
2024-01-12 11:55:43 +0500

    Implemneted the basic Authentiation in views

--- authenticationAndPermission/v1/api/views/basic_auth.py
+++ authenticationAndPermission/v1/api/views/basic_auth.py
@@ -1,8 +1,14 @@
 from api.models.student import Student
 from v1.api.serializers import StudentSerializer
 from rest_framework import viewsets
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated, AllowAny,IsAdminUser
 
 
 class StudentModelViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAdminUser]
+    # permission_classes = [AllowAny]
