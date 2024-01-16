8710768ef5c69b41409f5a52349bba60c7314240
Eman Amir <eman.amir@flicken.io>
2024-01-16 11:13:18 +0500

    Created Token authentication and a custom token generation

--- authenticationAndPermission/v1/api/urls.py
+++ authenticationAndPermission/v1/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from .views import *
 from rest_framework.routers import DefaultRouter
+from v1.api.auth import CustomAuthToken
 
 router = DefaultRouter()
 
@@ -8,5 +9,6 @@ router.register('studentapi', StudentModelViewSet, basename='student')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('gettoken/', CustomAuthToken.as_view())
 ]
--- authenticationAndPermission/v1/api/views/token_auth.py
+++ authenticationAndPermission/v1/api/views/token_auth.py
@@ -2,9 +2,11 @@ from api.models.student import Student
 from v1.api.serializers import StudentSerializer
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
-from v1.api.custompermissions import MyPermissions
+from rest_framework.authentication import TokenAuthentication
 
 
 class StudentModelViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
